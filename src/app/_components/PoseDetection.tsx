72d2d41e1ae227843fd343c73d511a0ca44a4ebb
nitipat.rstw <nitipat.rstw@gmail.com>
2024-03-02 16:42:06 +1100

    refactor: decouple init() and run() useEffect.

--- src/app/_components/PoseDetection.tsx
+++ src/app/_components/PoseDetection.tsx
@@ -1,23 +1,88 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import * as poseDetection from "@tensorflow-models/pose-detection";
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-backend-webgl";
-import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import debounce from "lodash/debounce";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
-const PoseDetection: React.FC = () => {
-  const [dimensions, setDimensions] = useState({ width: 640, height: 480 });
-  const [isLoading, setLoading] = useState(true);
-  const [currentPosture, setCurrentPosture] =
-    useState<poseDetection.Pose | null>(null);
-  const [idealPosture, setIdealPosture] = useState<poseDetection.Pose | null>();
-  const [postureStatus, setPostureStatus] = useState("");
+const PoseDetection = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [dimensions, setDimensions] = useState({ width: 640, height: 480 });
+  const [screenRatio, setScreenRatio] = useState<number>(
+    window.innerWidth / window.innerHeight
+  );
+  const [postureStatus, setPostureStatus] = useState("");
+  const [video, setVideo] = useState<HTMLVideoElement | null>(null);
+  const [detector, setDetector] = useState<poseDetection.PoseDetector | null>(
+    null
+  );
 
-  function onSaveIdealPosture() {
-    setIdealPosture(currentPosture);
-  }
+  useEffect(() => {
+    if (!video || !detector || video.width <= 0 || video.height <= 0) return;
+
+    const evaluatePosture = (pose: poseDetection.Pose | null) => {
+      if (!pose) {
+        setPostureStatus("No pose detected");
+        return;
+      }
+
+      const nose = pose.keypoints.find((k) => k.name === "nose");
+      const leftShoulder = pose.keypoints.find(
+        (k) => k.name === "left_shoulder"
+      );
+      const rightShoulder = pose.keypoints.find(
+        (k) => k.name === "right_shoulder"
+      );
+
+      if (!nose || !leftShoulder || !rightShoulder) {
+        setPostureStatus("Essential keypoints missing");
+        return;
+      }
+
+      const shoulderMidpointX = (leftShoulder.x + rightShoulder.x) / 2;
+      const horizontalDistance = Math.abs(nose.x - shoulderMidpointX);
+      const shoulderLevelDifference = Math.abs(
+        leftShoulder.y - rightShoulder.y
+      );
+
+      const maxLevelDifference = 10 * screenRatio;
+      const maxHorizontalDistance = 30 * screenRatio;
+
+      const isLeaningForward = horizontalDistance > maxHorizontalDistance;
+      const isTilting = shoulderLevelDifference > maxLevelDifference;
+
+      if (isLeaningForward && isTilting) {
+        setPostureStatus("Leaning forward and tilting");
+      } else if (isLeaningForward) {
+        setPostureStatus("Leaning forward");
+      } else if (isTilting) {
+        setPostureStatus("Tilting");
+      } else {
+        setPostureStatus("Good posture");
+      }
+    };
+
+    const detectPoseInRealTime = async () => {
+      const poseDetectionFrame = async () => {
+        const [pose] = await detector.estimatePoses(video);
+        evaluatePosture(pose);
+        requestAnimationFrame(poseDetectionFrame);
+      };
+
+      poseDetectionFrame();
+    };
+
+    const run = async () => {
+      video.play();
+      detectPoseInRealTime();
+      setLoading(false);
+    };
+
+    run();
+  }, [detector, screenRatio, video]);
 
   useEffect(() => {
     const setupCamera = async () => {
@@ -27,9 +92,9 @@ const PoseDetection: React.FC = () => {
         );
       }
       const video = videoRef.current;
+
       if (!video) return;
-      video.width = dimensions.width;
-      video.height = dimensions.height;
+
       video.srcObject = await navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {
@@ -50,6 +115,7 @@ const PoseDetection: React.FC = () => {
       const detectorConfig = {
         modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
       };
+
       const detector = await poseDetection.createDetector(
         poseDetection.SupportedModels.MoveNet,
         detectorConfig
@@ -58,123 +124,52 @@ const PoseDetection: React.FC = () => {
       return detector;
     };
 
-    const evaluatePosture = (pose: poseDetection.Pose | null) => {
-      if (!pose) {
-        setPostureStatus("No pose detected");
-        return;
-      }
-
-      // Assuming keypoints are available
-      const nose = pose.keypoints.find((k) => k.name === "nose");
-      const leftShoulder = pose.keypoints.find(
-        (k) => k.name === "left_shoulder"
-      );
-      const rightShoulder = pose.keypoints.find(
-        (k) => k.name === "right_shoulder"
-      );
-
-      if (!nose || !leftShoulder || !rightShoulder) {
-        setPostureStatus("Essential keypoints missing");
-        return;
-      }
-
-      const shoulderMidpointX = (leftShoulder.x + rightShoulder.x) / 2;
-      const horizontalDistance = Math.abs(nose.x - shoulderMidpointX);
-      const shoulderLevelDifference = Math.abs(
-        leftShoulder.y - rightShoulder.y
-      );
-
-      const maxLevelDifference = 10;
-      const maxHorizontalDistance = 30;
-
-      const isLeaningForward = horizontalDistance > maxHorizontalDistance;
-      const isTilting = shoulderLevelDifference > maxLevelDifference;
-
-      if (isLeaningForward && isTilting) {
-        setPostureStatus("Leaning forward and tilting");
-      } else if (isLeaningForward) {
-        setPostureStatus("Leaning forward");
-      } else if (isTilting) {
-        setPostureStatus("Tilting");
-      } else {
-        setPostureStatus("Good posture");
-      }
-    };
-
-    const detectPoseInRealTime = async (
-      video: HTMLVideoElement,
-      detector: poseDetection.PoseDetector
-    ) => {
-      const poseDetectionFrame = async () => {
-        const [pose] = await detector.estimatePoses(video);
-
-        setCurrentPosture(pose);
-
-        evaluatePosture(pose);
-
-        requestAnimationFrame(poseDetectionFrame);
-
-        setLoading(false);
-      };
-
-      poseDetectionFrame();
-    };
-
     const init = async () => {
       await tf.setBackend("webgl");
       await tf.ready();
 
       const video = await setupCamera();
+      const detector = await loadPoseModel();
 
       if (video) {
-        video.play();
+        setVideo(video);
+      }
 
-        const detector = await loadPoseModel();
-
-        detectPoseInRealTime(video, detector);
+      if (detector) {
+        setDetector(detector);
       }
     };
 
     init();
-  }, [dimensions.height, dimensions.width]);
+  }, []);
 
   useLayoutEffect(() => {
-    const updateDimensions = () => {
+    const updateDimensions = debounce(() => {
       const width = window.innerWidth;
-
+      const height = window.innerHeight;
       const targetWidth = width > 640 ? 640 : width;
       const targetHeight = (targetWidth * 480) / 640;
-
+      const newScreenRatio = width / height;
       setDimensions({ width: targetWidth, height: targetHeight });
-    };
-
+      setScreenRatio(newScreenRatio);
+    }, 100);
     updateDimensions();
-
     window.addEventListener("resize", updateDimensions);
-
     return () => window.removeEventListener("resize", updateDimensions);
   }, []);
 
   return (
     <>
-      {isLoading && <div>Loading...</div>}
-      {postureStatus}
-      <div className="relative">
-        <video ref={videoRef} className="scale-x-[-1]" />
+      {loading && <div>Loading...</div>}
+      <div className={cn(loading && "hidden")}>
+        <div className="text-center">Status: {postureStatus}</div>
+        <video
+          ref={videoRef}
+          className="scale-x-[-1]"
+          width={dimensions.width}
+          height={dimensions.height}
+        />
       </div>
-      <Button onClick={onSaveIdealPosture}>Save</Button>
-      {idealPosture &&
-        idealPosture.keypoints.map(({ x, y, name }) => {
-          if (
-            name &&
-            ["nose", "left_shoulder", "right_shoulder"].includes(name)
-          )
-            return (
-              <div key={name}>
-                {x} {y} {name}
-              </div>
-            );
-        })}
     </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import PoseDetection from "./_components/PoseDetection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <PoseDetection />
     </main>
   );
