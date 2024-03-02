1f6431bc47711cf82740f5efca31182733d29aad
nitipat.rstw <nitipat.rstw@gmail.com>
2024-03-03 09:30:17 +1100

    refactor: improve estimation logic, delete screen ratio factor.

--- src/app/_components/PoseDetection.tsx
+++ src/app/_components/PoseDetection.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import * as poseDetection from "@tensorflow-models/pose-detection";
+import {
+  Pose,
+  PoseDetector,
+  SupportedModels,
+  createDetector,
+  movenet,
+} from "@tensorflow-models/pose-detection";
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-backend-webgl";
 import debounce from "lodash/debounce";
@@ -11,58 +18,136 @@ const PoseDetection = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [loading, setLoading] = useState(true);
   const [dimensions, setDimensions] = useState({ width: 640, height: 480 });
-  const [screenRatio, setScreenRatio] = useState<number>(
-    window.innerWidth / window.innerHeight
-  );
   const [postureStatus, setPostureStatus] = useState("");
   const [video, setVideo] = useState<HTMLVideoElement | null>(null);
-  const [detector, setDetector] = useState<poseDetection.PoseDetector | null>(
-    null
-  );
+  const [detector, setDetector] = useState<PoseDetector | null>(null);
+  const [idealPosture, setIdealPosture] = useState<Pose | null>(null);
+  const [isBadPosture, setIsBadPosture] = useState(false);
+
+  const onSave = async () => {
+    if (!detector || !video) return;
+    const [pose] = await detector.estimatePoses(video);
+    setIdealPosture(pose);
+  };
 
   useEffect(() => {
-    if (!video || !detector || video.width <= 0 || video.height <= 0) return;
+    if (!video || !detector || !video.width || !video.height) return;
 
-    const evaluatePosture = (pose: poseDetection.Pose | null) => {
+    const evaluatePosture = (pose: Pose | null) => {
       if (!pose) {
         setPostureStatus("No pose detected");
         return;
       }
 
-      const nose = pose.keypoints.find((k) => k.name === "nose");
-      const leftShoulder = pose.keypoints.find(
-        (k) => k.name === "left_shoulder"
-      );
-      const rightShoulder = pose.keypoints.find(
-        (k) => k.name === "right_shoulder"
-      );
-
-      if (!nose || !leftShoulder || !rightShoulder) {
-        setPostureStatus("Essential keypoints missing");
+      if (!idealPosture) {
+        setPostureStatus(
+          "Please set your ideal posture now. Position yourself in the most comfortable, upright posture you believe is ideal for you. This will help us compare and improve your posture over time."
+        );
         return;
       }
 
-      const shoulderMidpointX = (leftShoulder.x + rightShoulder.x) / 2;
-      const horizontalDistance = Math.abs(nose.x - shoulderMidpointX);
-      const shoulderLevelDifference = Math.abs(
-        leftShoulder.y - rightShoulder.y
+      // Extract current keypoints
+      const noseCurrent = pose.keypoints.find((k) => k.name === "nose");
+      const leftShoulderCurrent = pose.keypoints.find(
+        (k) => k.name === "left_shoulder"
+      );
+      const rightShoulderCurrent = pose.keypoints.find(
+        (k) => k.name === "right_shoulder"
       );
 
-      const maxLevelDifference = 10 * screenRatio;
-      const maxHorizontalDistance = 30 * screenRatio;
+      // Extract ideal keypoints
+      const noseIdeal = idealPosture.keypoints.find((k) => k.name === "nose");
+      const leftShoulderIdeal = idealPosture.keypoints.find(
+        (k) => k.name === "left_shoulder"
+      );
+      const rightShoulderIdeal = idealPosture.keypoints.find(
+        (k) => k.name === "right_shoulder"
+      );
 
-      const isLeaningForward = horizontalDistance > maxHorizontalDistance;
-      const isTilting = shoulderLevelDifference > maxLevelDifference;
-
-      if (isLeaningForward && isTilting) {
-        setPostureStatus("Leaning forward and tilting");
-      } else if (isLeaningForward) {
-        setPostureStatus("Leaning forward");
-      } else if (isTilting) {
-        setPostureStatus("Tilting");
-      } else {
-        setPostureStatus("Good posture");
+      if (
+        !leftShoulderCurrent ||
+        !rightShoulderCurrent ||
+        !noseCurrent ||
+        !noseIdeal ||
+        !leftShoulderIdeal ||
+        !rightShoulderIdeal
+      ) {
+        setPostureStatus("Miss");
+        return;
       }
+
+      // Calculate current and ideal midpoints of shoulders
+      const shoulderMidpointCurrentX =
+        (leftShoulderCurrent.x + rightShoulderCurrent.x) / 2;
+      const shoulderMidpointIdealX =
+        (leftShoulderIdeal.x + rightShoulderIdeal.x) / 2;
+
+      // Horizontal distance to midpoint comparison
+      const horizontalDistanceCurrent = Math.abs(
+        noseCurrent.x - shoulderMidpointCurrentX
+      );
+      const horizontalDistanceIdeal = Math.abs(
+        noseIdeal.x - shoulderMidpointIdealX
+      );
+
+      // Shoulder level difference comparison
+      const shoulderLevelDifferenceCurrent = Math.abs(
+        leftShoulderCurrent.y - rightShoulderCurrent.y
+      );
+      const shoulderLevelDifferenceIdeal = Math.abs(
+        leftShoulderIdeal.y - rightShoulderIdeal.y
+      );
+
+      // Calculate the average shoulder height difference from the ideal
+      const averageShoulderHeightCurrent =
+        (leftShoulderCurrent.y + rightShoulderCurrent.y) / 2;
+      const averageShoulderHeightIdeal =
+        (leftShoulderIdeal.y + rightShoulderIdeal.y) / 2;
+
+      const HORIZONTAL_DIFFERENCE_THRESHOLD = 30;
+      const SHOULDER_LEVEL_DIFFERENCE_THRESHOLD = 15;
+      const SHOULDER_VERTICAL_THRESHOLD = 15;
+
+      const isShoulderHigher =
+        averageShoulderHeightCurrent + SHOULDER_VERTICAL_THRESHOLD <
+        averageShoulderHeightIdeal;
+      const isShouldersLower =
+        averageShoulderHeightCurrent >
+        averageShoulderHeightIdeal + SHOULDER_VERTICAL_THRESHOLD;
+      const isLeaningForward =
+        horizontalDistanceCurrent >
+          horizontalDistanceIdeal + HORIZONTAL_DIFFERENCE_THRESHOLD ||
+        isShoulderHigher;
+      const isTilting =
+        shoulderLevelDifferenceCurrent >
+        shoulderLevelDifferenceIdeal + SHOULDER_LEVEL_DIFFERENCE_THRESHOLD;
+
+      if (!isShouldersLower && !isLeaningForward && !isTilting) {
+        setPostureStatus("Good posture. Keep it up!");
+        setIsBadPosture(false);
+        return; // Exit early if good posture
+      }
+
+      setIsBadPosture(true);
+      let postureMessage = "Adjust your posture: ";
+      if (isShouldersLower && isLeaningForward) {
+        postureMessage =
+          "Your shoulders are significantly lower, and you're leaning forward too much. Try to sit up straight and bring your shoulders back.";
+      } else if (isShouldersLower && isTilting) {
+        postureMessage =
+          "Your shoulders are significantly lower, and your posture is tilted. Try to straighten your back and level your shoulders.";
+      } else if (isLeaningForward) {
+        postureMessage =
+          "You're leaning forward too much. Try to align your head over your shoulders and sit up straight.";
+      } else if (isTilting) {
+        postureMessage =
+          "Your posture is tilted. Make sure to keep your shoulders level.";
+      } else if (isShouldersLower) {
+        postureMessage =
+          "Your shoulders are significantly lower than the ideal position. Try lifting them slightly and sitting up straight.";
+      }
+
+      setPostureStatus(postureMessage);
     };
 
     const detectPoseInRealTime = async () => {
@@ -82,7 +167,7 @@ const PoseDetection = () => {
     };
 
     run();
-  }, [detector, screenRatio, video]);
+  }, [detector, idealPosture, video]);
 
   useEffect(() => {
     const setupCamera = async () => {
@@ -113,11 +198,11 @@ const PoseDetection = () => {
 
     const loadPoseModel = async () => {
       const detectorConfig = {
-        modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+        modelType: movenet.modelType.SINGLEPOSE_LIGHTNING,
       };
 
-      const detector = await poseDetection.createDetector(
-        poseDetection.SupportedModels.MoveNet,
+      const detector = await createDetector(
+        SupportedModels.MoveNet,
         detectorConfig
       );
 
@@ -146,12 +231,9 @@ const PoseDetection = () => {
   useLayoutEffect(() => {
     const updateDimensions = debounce(() => {
       const width = window.innerWidth;
-      const height = window.innerHeight;
       const targetWidth = width > 640 ? 640 : width;
       const targetHeight = (targetWidth * 480) / 640;
-      const newScreenRatio = width / height;
       setDimensions({ width: targetWidth, height: targetHeight });
-      setScreenRatio(newScreenRatio);
     }, 100);
     updateDimensions();
     window.addEventListener("resize", updateDimensions);
@@ -169,6 +251,7 @@ const PoseDetection = () => {
           width={dimensions.width}
           height={dimensions.height}
         />
+        <Button onClick={onSave}>Save Ideal Posture</Button>
       </div>
     </>
   );
