17977293fdc07f9dc006f1c4bf5e5201e01f4db3
nitipat.rstw <nitipat.rstw@gmail.com>
2024-03-03 13:03:22 +1100

    feat: add notification, loading spinner, local https.

--- src/app/_components/PoseDetection.tsx
+++ src/app/_components/PoseDetection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import { cn } from "@/lib/utils";
 import {
   Pose,
@@ -33,145 +34,46 @@ const PoseDetection = () => {
     setIdealPosture(pose);
   };
 
+  // init notification permission
   useEffect(() => {
-    if (!video || !detector || !video.width || !video.height) return;
+    if (
+      Notification.permission !== "granted" &&
+      Notification.permission !== "denied"
+    ) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("Notification permission granted.");
+        }
+      });
+    }
+  }, []);
 
-    const evaluatePosture = (pose: Pose | null) => {
-      if (!pose) {
-        setPostureStatus("No pose detected");
-        return;
+  // notification
+  useEffect(() => {
+    if (isBadPosture && !badPostureTimeoutId) {
+      const timeoutId = setTimeout(() => {
+        if ("Notification" in window && Notification.permission === "granted") {
+          new Notification("Adjust Your Posture", {
+            body: "You've been in a bad posture for a while. Time to straighten up!",
+          });
+          setBadPostureTimeoutId(null);
+        }
+      }, 10000);
+
+      setBadPostureTimeoutId(timeoutId);
+    } else if (!isBadPosture && badPostureTimeoutId) {
+      clearTimeout(badPostureTimeoutId);
+      setBadPostureTimeoutId(null);
+    }
+
+    return () => {
+      if (badPostureTimeoutId) {
+        clearTimeout(badPostureTimeoutId);
       }
-
-      if (!idealPosture) {
-        setPostureStatus(
-          "Please set your ideal posture now. Position yourself in the most comfortable, upright posture you believe is ideal for you. This will help us compare and improve your posture over time."
-        );
-        return;
-      }
-
-      // Extract current keypoints
-      const noseCurrent = pose.keypoints.find((k) => k.name === "nose");
-      const leftShoulderCurrent = pose.keypoints.find(
-        (k) => k.name === "left_shoulder"
-      );
-      const rightShoulderCurrent = pose.keypoints.find(
-        (k) => k.name === "right_shoulder"
-      );
-
-      // Extract ideal keypoints
-      const noseIdeal = idealPosture.keypoints.find((k) => k.name === "nose");
-      const leftShoulderIdeal = idealPosture.keypoints.find(
-        (k) => k.name === "left_shoulder"
-      );
-      const rightShoulderIdeal = idealPosture.keypoints.find(
-        (k) => k.name === "right_shoulder"
-      );
-
-      if (
-        !leftShoulderCurrent ||
-        !rightShoulderCurrent ||
-        !noseCurrent ||
-        !noseIdeal ||
-        !leftShoulderIdeal ||
-        !rightShoulderIdeal
-      ) {
-        setPostureStatus("Miss");
-        return;
-      }
-
-      // Calculate current and ideal midpoints of shoulders
-      const shoulderMidpointCurrentX =
-        (leftShoulderCurrent.x + rightShoulderCurrent.x) / 2;
-      const shoulderMidpointIdealX =
-        (leftShoulderIdeal.x + rightShoulderIdeal.x) / 2;
-
-      // Horizontal distance to midpoint comparison
-      const horizontalDistanceCurrent = Math.abs(
-        noseCurrent.x - shoulderMidpointCurrentX
-      );
-      const horizontalDistanceIdeal = Math.abs(
-        noseIdeal.x - shoulderMidpointIdealX
-      );
-
-      // Shoulder level difference comparison
-      const shoulderLevelDifferenceCurrent = Math.abs(
-        leftShoulderCurrent.y - rightShoulderCurrent.y
-      );
-      const shoulderLevelDifferenceIdeal = Math.abs(
-        leftShoulderIdeal.y - rightShoulderIdeal.y
-      );
-
-      // Calculate the average shoulder height difference from the ideal
-      const averageShoulderHeightCurrent =
-        (leftShoulderCurrent.y + rightShoulderCurrent.y) / 2;
-      const averageShoulderHeightIdeal =
-        (leftShoulderIdeal.y + rightShoulderIdeal.y) / 2;
-
-      const HORIZONTAL_DIFFERENCE_THRESHOLD = 60;
-      const SHOULDER_LEVEL_DIFFERENCE_THRESHOLD = 30;
-      const SHOULDER_VERTICAL_THRESHOLD = 30;
-
-      const isShoulderHigher =
-        averageShoulderHeightCurrent + SHOULDER_VERTICAL_THRESHOLD <
-        averageShoulderHeightIdeal;
-      const isShouldersLower =
-        averageShoulderHeightCurrent >
-        averageShoulderHeightIdeal + SHOULDER_VERTICAL_THRESHOLD;
-      const isLeaningForward =
-        horizontalDistanceCurrent >
-          horizontalDistanceIdeal + HORIZONTAL_DIFFERENCE_THRESHOLD ||
-        isShoulderHigher;
-      const isTilting =
-        shoulderLevelDifferenceCurrent >
-        shoulderLevelDifferenceIdeal + SHOULDER_LEVEL_DIFFERENCE_THRESHOLD;
-
-      if (!isShouldersLower && !isLeaningForward && !isTilting) {
-        setPostureStatus("Good posture. Keep it up!");
-        setIsBadPosture(false);
-        return;
-      }
-
-      setIsBadPosture(true);
-      let postureMessage = "Adjust your posture: ";
-      if (isShouldersLower && isLeaningForward) {
-        postureMessage =
-          "Your shoulders are significantly lower, and you're leaning forward too much. Try to sit up straight and bring your shoulders back.";
-      } else if (isShouldersLower && isTilting) {
-        postureMessage =
-          "Your shoulders are significantly lower, and your posture is tilted. Try to straighten your back and level your shoulders.";
-      } else if (isLeaningForward) {
-        postureMessage =
-          "You're leaning forward too much. Try to align your head over your shoulders and sit up straight.";
-      } else if (isTilting) {
-        postureMessage =
-          "Your posture is tilted. Make sure to keep your shoulders level.";
-      } else if (isShouldersLower) {
-        postureMessage =
-          "Your shoulders are significantly lower than the ideal position. Try lifting them slightly and sitting up straight.";
-      }
-
-      setPostureStatus(postureMessage);
     };
+  }, [isBadPosture, badPostureTimeoutId]);
 
-    const detectPoseInRealTime = async () => {
-      const poseDetectionFrame = async () => {
-        const [pose] = await detector.estimatePoses(video);
-        evaluatePosture(pose);
-        requestAnimationFrame(poseDetectionFrame);
-      };
-
-      poseDetectionFrame();
-    };
-
-    const run = async () => {
-      video.play();
-      detectPoseInRealTime();
-      setLoading(false);
-    };
-
-    run();
-  }, [detector, idealPosture, video]);
-
+  // init camera and load model
   useEffect(() => {
     const setupCamera = async () => {
       if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -231,6 +133,116 @@ const PoseDetection = () => {
     init();
   }, []);
 
+  // run to evalute posture
+  useEffect(() => {
+    if (!video || !detector || !video.width || !video.height) return;
+
+    const evaluatePosture = (pose: Pose | null) => {
+      if (!pose || !idealPosture) {
+        return;
+      }
+
+      // Extract current keypoints
+      const noseCurrent = pose.keypoints.find((k) => k.name === "nose");
+      const leftShoulderCurrent = pose.keypoints.find(
+        (k) => k.name === "left_shoulder"
+      );
+      const rightShoulderCurrent = pose.keypoints.find(
+        (k) => k.name === "right_shoulder"
+      );
+
+      // Extract ideal keypoints
+      const noseIdeal = idealPosture.keypoints.find((k) => k.name === "nose");
+      const leftShoulderIdeal = idealPosture.keypoints.find(
+        (k) => k.name === "left_shoulder"
+      );
+      const rightShoulderIdeal = idealPosture.keypoints.find(
+        (k) => k.name === "right_shoulder"
+      );
+
+      if (
+        !leftShoulderCurrent ||
+        !rightShoulderCurrent ||
+        !noseCurrent ||
+        !noseIdeal ||
+        !leftShoulderIdeal ||
+        !rightShoulderIdeal
+      ) {
+        return;
+      }
+
+      // Calculate current and ideal midpoints of shoulders
+      const shoulderMidpointCurrentX =
+        (leftShoulderCurrent.x + rightShoulderCurrent.x) / 2;
+      const shoulderMidpointIdealX =
+        (leftShoulderIdeal.x + rightShoulderIdeal.x) / 2;
+
+      // Horizontal distance to midpoint comparison
+      const horizontalDistanceCurrent = Math.abs(
+        noseCurrent.x - shoulderMidpointCurrentX
+      );
+      const horizontalDistanceIdeal = Math.abs(
+        noseIdeal.x - shoulderMidpointIdealX
+      );
+
+      // Shoulder level difference comparison
+      const shoulderLevelDifferenceCurrent = Math.abs(
+        leftShoulderCurrent.y - rightShoulderCurrent.y
+      );
+      const shoulderLevelDifferenceIdeal = Math.abs(
+        leftShoulderIdeal.y - rightShoulderIdeal.y
+      );
+
+      // Calculate the average shoulder height difference from the ideal
+      const averageShoulderHeightCurrent =
+        (leftShoulderCurrent.y + rightShoulderCurrent.y) / 2;
+      const averageShoulderHeightIdeal =
+        (leftShoulderIdeal.y + rightShoulderIdeal.y) / 2;
+
+      const HORIZONTAL_DIFFERENCE_THRESHOLD = 60;
+      const SHOULDER_LEVEL_DIFFERENCE_THRESHOLD = 30;
+      const SHOULDER_VERTICAL_THRESHOLD = 30;
+
+      const isShoulderHigher =
+        averageShoulderHeightCurrent + SHOULDER_VERTICAL_THRESHOLD <
+        averageShoulderHeightIdeal;
+      const isShouldersLower =
+        averageShoulderHeightCurrent >
+        averageShoulderHeightIdeal + SHOULDER_VERTICAL_THRESHOLD;
+      const isLeaningForward =
+        horizontalDistanceCurrent >
+          horizontalDistanceIdeal + HORIZONTAL_DIFFERENCE_THRESHOLD ||
+        isShoulderHigher;
+      const isTilting =
+        shoulderLevelDifferenceCurrent >
+        shoulderLevelDifferenceIdeal + SHOULDER_LEVEL_DIFFERENCE_THRESHOLD;
+
+      if (!isShouldersLower && !isLeaningForward && !isTilting) {
+        setIsBadPosture(false);
+      } else {
+        setIsBadPosture(true);
+      }
+    };
+
+    const detectPoseInRealTime = async () => {
+      const poseDetectionFrame = async () => {
+        const [pose] = await detector.estimatePoses(video);
+        evaluatePosture(pose);
+        requestAnimationFrame(poseDetectionFrame);
+      };
+
+      poseDetectionFrame();
+    };
+
+    const run = async () => {
+      video.play();
+      detectPoseInRealTime();
+      setLoading(false);
+    };
+
+    run();
+  }, [detector, idealPosture, video]);
+
   useLayoutEffect(() => {
     const updateDimensions = debounce(() => {
       const width = window.innerWidth;
@@ -244,70 +256,79 @@ const PoseDetection = () => {
   }, []);
 
   return (
-    <div
-      className={cn(
-        "flex flex-col gap-6 items-center max-w-[40rem]",
-        loading && "hidden"
-      )}
-    >
-      <p className={cn("leading-7 mr-auto", idealPosture && "hidden")}>
-        Please set your ideal posture now. Position yourself in the most
-        comfortable, upright posture you believe is ideal for you. This will
-        help us compare and improve your posture over time.
-      </p>
-
-      <div className="relative">
-        <div
-          className={cn(
-            "hidden opacity-0",
-            idealPosture && "block",
-            isStartSession && "opacity-100"
-          )}
-        >
-          {isBadPosture ? (
-            <p className="text-center font-extrabold text-red-700">
-              BAD POSTURE
-            </p>
-          ) : (
-            <p className="text-center font-extrabold text-green-700">
-              GOOD POSTURE
-            </p>
-          )}
-        </div>
-        <video
-          ref={videoRef}
-          className={cn(
-            "scale-x-[-1] rounded-2xl border-4 border-black",
-            isStartSession &&
-              (isBadPosture ? "border-red-600" : "border-green-600")
-          )}
-          width={dimensions.width}
-          height={dimensions.height}
-        />
+    <>
+      <div className="flex justify-center">
+        {loading && <Spinner className="w-60 h-60 mt-32" />}
       </div>
-      {idealPosture ? (
-        <div className="flex gap-4">
-          {isStartSession ? (
-            <Button onClick={() => setIsStartSession(false)}>
-              End Session
-            </Button>
-          ) : (
-            <>
-              <Button onClick={() => setIsStartSession(true)}>
-                Start Session
-              </Button>
-              <Button variant="outline" onClick={() => setIdealPosture(null)}>
-                Reset Ideal Posture
-              </Button>
-            </>
-          )}
+      <div
+        className={cn(
+          "flex flex-col gap-6 items-center max-w-[40rem]",
+          loading && "hidden"
+        )}
+      >
+        <p className={cn("leading-7 mr-auto", idealPosture && "hidden")}>
+          Please set your ideal posture now. Position yourself in the most
+          comfortable, upright posture you believe is ideal for you. This will
+          help us compare and improve your posture over time.
+        </p>
+
+        <div className="relative">
+          <div
+            className={cn(
+              "hidden opacity-0",
+              idealPosture && "block",
+              isStartSession && "opacity-100"
+            )}
+          >
+            {isBadPosture ? (
+              <p className="text-center font-extrabold text-red-700">
+                BAD POSTURE
+              </p>
+            ) : (
+              <p className="text-center font-extrabold text-green-700">
+                GOOD POSTURE
+              </p>
+            )}
+          </div>
+          <video
+            ref={videoRef}
+            className={cn(
+              "scale-x-[-1] rounded-2xl border-4 border-black",
+              isStartSession &&
+                (isBadPosture ? "border-red-600" : "border-green-600")
+            )}
+            width={dimensions.width}
+            height={dimensions.height}
+          />
         </div>
-      ) : (
-        <Button className="w-fit" onClick={onSave}>
-          Save Ideal Posture
-        </Button>
-      )}
-    </div>
+        {idealPosture ? (
+          <div className="flex gap-4">
+            {isStartSession ? (
+              <Button onClick={() => setIsStartSession(false)} size="lg">
+                End Session
+              </Button>
+            ) : (
+              <>
+                <Button size="lg" onClick={() => setIsStartSession(true)}>
+                  Start Session
+                </Button>
+                <Button
+                  size="lg"
+                  variant="outline"
+                  onClick={() => setIdealPosture(null)}
+                >
+                  Reset Ideal Posture
+                </Button>
+              </>
+            )}
+          </div>
+        ) : (
+          <Button size="lg" className="w-fit" onClick={onSave}>
+            Save Ideal Posture
+          </Button>
+        )}
+      </div>
+    </>
   );
 };
 
