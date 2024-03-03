058ff7f1fda2a0ded56127488dd89d868f490a08
nitipat.rstw <nitipat.rstw@gmail.com>
2024-03-03 15:40:02 +1100

    fix: build error.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-export const fontSans = Inter({
+const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
